42b5bdc27e4653d8430899c8acdfa516e50ca465
Tormod Liseth <tormod@sifterstudios.no>
2021-05-13 18:17:55 +0200

    Cached _rb in awake

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,16 +1,29 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] float _moveSpeed = 2;
+    float _jumpForce = 400;
+    Rigidbody2D _rb;
 
-    void Update()
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, vertical);
+        Vector3 movement = new Vector3(horizontal, 0);
 
-        transform.position += movement * Time.deltaTime * _moveSpeed;
+        transform.position += movement * (Time.deltaTime * _moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _rb.AddForce(Vector2.up * _jumpForce);
+        }
     }
 }
